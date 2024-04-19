47dfd248afd0d06385bf83940784504927a3ebd3
Jonathan <suarezpadillajonathan@gmail.com>
2024-04-19 13:15:58 -0500

    Se crea la interfaz CRUD

--- src/main/java/com/riwi/To_do_list/entity/Task.java
+++ src/main/java/com/riwi/To_do_list/entity/Task.java
@@ -26,4 +26,63 @@ public class Task {
     @Column(length = 50, nullable = false)
     private String status;
 
+    public Task() { }
+
+    public Task(Long id, String title, LocalDate creationDate, LocalTime creationTime, String status) {        this.id = id;
+        this.title = title;
+        this.creationDate = creationDate;
+        this.creationTime = creationTime;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalTime getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(LocalTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task\n" +
+                "id: " + id +
+                ", title: " + title +
+                ", creation date: " + creationDate +
+                ", creation time: " + creationTime +
+                ", status: " + status +
+               "\n-----------------------------------------------\n";
+    }
 }
